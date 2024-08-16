f14b1c2dff6f3cf0c012d51a71c462a0b3dba378
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-16 15:09:34 -0700

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,23 @@
+// changing-element-of-date-and-time
+
+// old stuff
+
+// const now = new Date();
+// console.log(now.getFullYear());
+
+// set date 
+// const now = new Date();
+// console.log({now});
+// console.log("before setting: ", now.getFullYear());
+
+// now.setFullYear("2035")
+// console.log({now});
+// console.log("after setting: ", now.getFullYear());
+
+// set hour
+// const now = new Date();
+// // before setting
+// console.log("before setting: ", now.getHours());
+// // after setting
+// now.setHours("22")
+// console.log("after setting: ", now.getHours());
